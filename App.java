006e7a30a69591ddac3c95d290c3ac87f3a2da6e
quangminh141005 <minhnq.23bi14288@usth.edu.vn>
2025-05-21 09:43:15 +0700

    add snake game window and panel

---
+++ App.java
@@ -0,0 +1,22 @@
+import javax.swing.*; // For creating a window
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int BoardWidth = 600;
+        int BoardHeight = BoardWidth;
+
+        // Create a new window
+        JFrame frame = new JFrame("Snake");
+  
+        frame.setSize(BoardWidth, BoardHeight);
+        frame.setLocationRelativeTo(null); // Set the default location of the window to the middle
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close when user click on the close button
+        frame.setVisible(true);
+
+        SnakeGame snakeGame = new SnakeGame(BoardWidth, BoardHeight);
+        frame.add(snakeGame);   
+        frame.pack(); //Does not include the title of the window included in the size
+    }
+}
---
+++ SnakeGame.java
@@ -0,0 +1,69 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+public class SnakeGame extends JPanel {
+
+    private class Tile {
+        int x; 
+        int y;
+        Tile(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    
+    int BoardWidth;
+    int BoardHeight;
+    int tileSize = 25; // The panel contain 24 pixel
+
+    // Food
+    Tile food;
+
+    // Snake
+    Tile snakeHead;
+    Ramdom random;
+
+    SnakeGame(int BoardWidth, int BoardHeight) { // Creating a panel for the window
+        this.BoardWidth = BoardWidth;
+        this.BoardHeight = BoardHeight;
+        setPreferredSize(new Dimension(this.BoardWidth, this.BoardHeight));
+        setBackground(Color.black);
+
+        snakeHead = new Tile(5,5);
+
+        food = new Tile(10, 10);
+
+        random = new Random();
+        placefood();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        // Grid 
+        for (int i = 0; i < BoardWidth/tileSize; i++) {
+            g.drawLine(i*tileSize, 0, i*tileSize, BoardHeight); // Draw vertical line
+            g.drawLine(0, i*tileSize, BoardWidth, i*tileSize); // Draw horizontal line
+        }
+
+        // Food
+        g.setColor(Color.red);
+        g.fillRect(food.x * tileSize, food.y * tileSize, tileSize, tileSize);
+
+
+        // Snake
+        g.setColor(Color.green);;
+        g.fillRect(snakeHead.x * tileSize, snakeHead.y * tileSize, tileSize, tileSize);
+    }
+
+    public void placeFood() {
+        food.x = random.nextInt(BoardWidth/tileSize); // 600/25 = 24 (pixel)
+        food.y = random.netInt(BoardHeight/tileSize);
+    }
+}
